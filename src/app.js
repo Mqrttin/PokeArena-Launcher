00b1362b9187df5b37877db02df11349ccd33983
Mqrttin <163344341+Mqrttin@users.noreply.github.com>
2025-11-22 14:49:09 -0300

    V-1.0.9

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ function quitLauncher() {
 const { status } = require('minecraft-server-util');
 
 function actualizarJugadoresRPC() {
-    status('45.63.108.27', 25530) // usa el puerto de conexión normal del servidor
+    status('172.96.172.240', 25530) // usa el puerto de conexión normal del servidor
         .then((response) => {
             console.log('Estado del servidor:', response);
 
